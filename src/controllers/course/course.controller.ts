b3f23010cd74bce65c1d1358f71e724733ad29e2
Mohammed Moataz <imohammedmoataz@gmail.com>
2024-10-05 01:38:14 +0300

    finished course apis

--- src/controllers/course/course.controller.ts
+++ src/controllers/course/course.controller.ts
@@ -7,54 +7,70 @@ import {
     Post,
     Put,
     Query,
+    UseGuards,
     UseInterceptors,
+    UsePipes,
+    ValidationPipe,
 } from '@nestjs/common'
 import {
     ApiBearerAuth,
     ApiTags
 } from '@nestjs/swagger'
+import JwtAuthGuard from 'src/auth/guards/jwt.guard'
 
 import { CreateCourseDto, UpdateCourseDto } from 'src/DTO/course.dto'
 import { CreateCourseInterceptor } from 'src/interceptors/course.interceptor'
 import { CourseService } from 'src/services/course/course.service'
 
 @ApiTags("Course APIs")
-// @ApiBearerAuth('JWT')
+@ApiBearerAuth('JWT')
 @Controller()
 export class CourseController {
     constructor(private CourseService: CourseService) { }
 
     @UseInterceptors(ClassSerializerInterceptor)
     @Get('course/all')
+    @UsePipes(ValidationPipe)
+    @UseGuards(JwtAuthGuard)
     getCoursess() {
         return this.CourseService.findAll()
     }
 
     @ApiTags("User APIs")
-    @UseInterceptors(ClassSerializerInterceptor)
     @Get('user/courses')
+    @UseInterceptors(ClassSerializerInterceptor)
+    @UsePipes(ValidationPipe)
+    @UseGuards(JwtAuthGuard)
     getTeacherCoursess(@Query('teacher_id') teacher_id: string) {
         return this.CourseService.findAllByTeacher(teacher_id)
     }
 
-    @UseInterceptors(ClassSerializerInterceptor)
     @Get('course')
+    @UseInterceptors(ClassSerializerInterceptor)
+    @UsePipes(ValidationPipe)
+    @UseGuards(JwtAuthGuard)
     getCourse(@Query('id') id: string) {
         return this.CourseService.findOneById(id)
     }
 
     @Post('course')
     @UseInterceptors(CreateCourseInterceptor)
+    @UsePipes(ValidationPipe)
+    @UseGuards(JwtAuthGuard)
     createCourse(@Body() newCourses: CreateCourseDto) {
         return this.CourseService.create(newCourses)
     }
 
     @Put('course')
+    @UsePipes(ValidationPipe)
+    @UseGuards(JwtAuthGuard)
     updatecourse(@Query('id') id: string, @Body() updatedCourses: UpdateCourseDto) {
         return this.CourseService.update(id, updatedCourses)
     }
 
     @Delete('course')
+    @UsePipes(ValidationPipe)
+    @UseGuards(JwtAuthGuard)
     removeCourse(@Query('id') id: string) {
         return this.CourseService.remove(id)
     }
--- src/services/course/course.service.ts
+++ src/services/course/course.service.ts
@@ -6,6 +6,7 @@ import { plainToClass } from 'class-transformer'
 import { Course } from 'src/entities/course.entity'
 import { CourseDto, CreateCourseDto, UpdateCourseDto } from 'src/DTO/course.dto'
 import { User } from 'src/entities/user.entity'
+import { ROLES } from 'src/utils/constants'
 
 @Injectable()
 export class CourseService {
@@ -57,13 +58,17 @@ export class CourseService {
     }
 
     async create(newCourse: CreateCourseDto): Promise<Course> {
-        let user = await this.userRepository.findOneBy({ uuid: newCourse.teacher_uuid })
-        newCourse["teacher_id"] = user.id
+        const teacher = await this.userRepository.findOneBy({ uuid: newCourse.teacher_uuid })
 
-        if (user && user._deleted_at === null)
+        if (!teacher || teacher.role !== ROLES.TEACHER)
+            throw new NotFoundException(`Teacher not found`)
+
+        newCourse["teacher_id"] = teacher.id
+
+        if (teacher && teacher._deleted_at === null)
             return await this.courseRepository.save(newCourse)
         else
-            throw new NotFoundException(`User with id: ${newCourse.teacher_uuid} not found`)
+            throw new NotFoundException(`Teacher with id: ${newCourse.teacher_uuid} not found`)
     }
 
     async update(id: string, updatedCourse: UpdateCourseDto): Promise<any> {
@@ -71,11 +76,29 @@ export class CourseService {
             uuid: id,
             _deleted_at: IsNull()
         })
+        const teacher = await this.userRepository.findOneBy({
+            uuid: updatedCourse.teacher_uuid,
+            _deleted_at: IsNull()
+        })
 
         if (!course)
             throw new NotFoundException(`Course with id: ${id} not found`)
 
-        return await this.courseRepository.update({ uuid: id }, updatedCourse)
+        if (teacher) {
+            if (teacher.role !== ROLES.TEACHER)
+                throw new NotFoundException(`Teacher not found`)
+
+            const result = await this.courseRepository.update({ uuid: id }, {
+                name: updatedCourse.name,
+                description: updatedCourse.description,
+                teacher_id: teacher.id,
+                _updated_at: new Date()
+            })
+
+            if (result) return "Course was updated successfully"
+        } else {
+            throw new NotFoundException(`Teacher not found`)
+        }
     }
 
     async remove(id: string): Promise<any> {
